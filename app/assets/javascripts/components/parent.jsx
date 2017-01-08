5f5ade8c630e09df674a2575b4bec38b93fce7e5
Alex Giacobbe <agiacobbe18@gmail.com>
2017-01-08 17:07:24 -0500

    add gems and stuff

--- app/assets/javascripts/components/parent.jsx
+++ app/assets/javascripts/components/parent.jsx
@@ -1,19 +1,32 @@
 class Parent extends React.Component {
 
   handleSubmit(e) {
-  debugger
-    console.log("worked")
-  }
+    e.preventDefault()
+    let urlBase = "https://api.fullcontact.com/v2/person.json?email="
+    let inputText = $("#inputText").val();
+    urlBase = urlBase+"bart@fullcontact.com"    
+
+    $.ajax({
+      url: "https://api.fullcontact.com/v2/person.json?email=bart@fullcontact.com",
+      method: 'GET',
+      headers: { 'X-FullContact-APIKey': 'empty',
+      "Access-Control-Allow-Credentials": true
+       },
+  }).done(function(response){
+    debugger
+    console.log(response)
+  })
+}
 
 render() {
   return (
 
       <div className="jumbotron">
-        <h1>Text appears here:</h1>
+        <h1>Write Text Here:</h1>
           <div className="form-group">
-            <textarea className="form-control" rows="5" id="comment"></textarea>
+            <textarea className="form-control" rows="5" id="inputText"></textarea>
           </div>
-        <p><button className="btn btn-lg btn-success blue" role="button" onClick={this.handleSubmit}>Press to Start Recording</button></p>
+        <p><button className="btn btn-lg btn-success blue" role="button" onClick={this.handleSubmit}>Submit</button></p>
       </div>
 
   )
